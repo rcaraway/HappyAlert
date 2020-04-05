e3c0f4ce342746a6fb92a2545c3210566fcce0ea
Rob Caraway <robbor911@gmail.com>
2020-04-04 22:35:34 -0500

    Initial Commit

---
+++ Sources/HappyAlert/HappyAlert.swift
@@ -0,0 +1,3 @@
+struct HappyAlert {
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import HappyAlertTests
+
+var tests = [XCTestCaseEntry]()
+tests += HappyAlertTests.allTests()
+XCTMain(tests)
