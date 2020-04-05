9bb9034fff5e609a2b21bee746e397ad6b852ea6
Rob Caraway <robbor911@gmail.com>
2020-04-04 22:52:27 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # HappyAlert
 
-A description of this package.
+Quick way to show standard alerts. 
+
+## Install with Swift Package Manager
+
+## How to use: 
+
+**Within a `UIViewController`:**
+
+To show a simple message: 
+
+`self.showAlert(title: "Message Sent", message: nil)`
+
+
+Show with a `cancel` or `OK` action: 
+
+```
+self.showAlert(title: "Send Message?", message: nil) {
+   self.sendMessage() //this block called when confirmed
+}
--- Sources/HappyAlert/HappyAlert.swift
+++ Sources/HappyAlert/HappyAlert.swift
@@ -1,3 +1,37 @@
-struct HappyAlert {
-    var text = "Hello, World!"
+#if !os(macOS)
+
+import UIKit
+
+public extension UIViewController {
+    @discardableResult
+    func showAlert(title: String?, message: String?) -> UIAlertController {
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak self] (action) in
+            self?.dismiss(animated: true, completion: nil)
+        }))
+        self.present(alert, animated: true, completion: nil)
+        return alert
+    }
+    
+    @discardableResult
+    func showAlert(title: String?, message: String?, cancelTitle: String = "Cancel", actionTitle: String = "OK", _ actionBlock: @escaping () -> Void) -> UIAlertController {
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: cancelTitle, style: .cancel, handler: { [weak self] (action) in
+            self?.dismiss(animated: true, completion: nil)
+        }))
+        alert.addAction(UIAlertAction(title: actionTitle, style: .default, handler: { [weak self] (action) in
+            if self != nil {
+                actionBlock()
+            }
+        }))
+        self.present(alert, animated: true, completion: nil)
+        return alert
+    }
+    
 }
+
+#endif
